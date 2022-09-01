2a05693ab3b7e6747b51a2dd574e01675731cca6
Pache <agustindiazmclh@gmail.com>
2022-09-01 20:40:47 -0300

    primeraentrega

---
+++ ACJ/js/alert.js
@@ -0,0 +1,8 @@
+document.getElementById('NA').addEventListener('click', function (e) {
+  e.preventDefault();
+  alert('No disponible todavia');
+});
+document.getElementById('NA2').addEventListener('click', function (e) {
+  e.preventDefault();
+  alert('No disponible todavia');
+});
