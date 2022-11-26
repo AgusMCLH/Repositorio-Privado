8cfdd106bc5bc6d413ec5288d06b4ef17fe766c4
Pache <agustindiazmclh@gmail.com>
2022-11-26 10:03:17 -0300

    NA

--- Desarrollo Web/Preentrega2Diaz/ACJ/js/alert.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('NA').addEventListener('click', function (e) {
-  e.preventDefault();
-  alert('No disponible todavia');
-});
-document.getElementById('NA2').addEventListener('click', function (e) {
-  e.preventDefault();
-  alert('No disponible todavia');
-});
