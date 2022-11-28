da009682306f951ed6ed9d90bf3c1b49639a465c
Pache <agustindiazmclh@gmail.com>
2022-11-28 15:33:16 -0300

    Segunda entrega de JS CoderHouse Completada

--- Java Script/Segunda entrega/js/index.js
+++ Java Script/Segunda entrega/js/index.js
@@ -14,6 +14,7 @@ let productos = [];
 let checkedCamps = 0;
 let divProductos = document.getElementsByClassName('productos');
 divProductos = divProductos[0];
+let botonesComprar = [];
 
 //Funcion del boton collapse
 collapseButton.addEventListener('click', () => {
@@ -94,13 +95,22 @@ addButton.addEventListener('click', () => {
     if (validarNumeros(listaInputsNumber[i])) {
       checkedCamps++;
     } else {
-      console.log(listaInputsNumber[i] + 'no cumple');
+      showErrorCard(
+        `Error en el campo ${listaInputsNumber[i].id}`,
+        'El campo solo admite numeros'
+      );
     }
   }
   if (productDiscount.value <= 100 && productDiscount.value >= 0) {
     if (checkedCamps === listaInputsLetras.concat(listaInputsNumber).length) {
       agregar();
+    } else {
     }
+  } else {
+    showErrorCard(
+      `Error en el campo ${productDiscount.id}`,
+      'El campo solo admite numeros entre 0 y 100'
+    );
   }
 });
 
@@ -135,6 +145,12 @@ const agregar = () => {
     category: productCategory.value,
   });
   showProducts();
+  botonesComprar = document.getElementsByClassName('buy');
+  for (let i = 0; i < botonesComprar.length; i++) {
+    botonesComprar[i].addEventListener('click', () => {
+      alert('No disp.');
+    });
+  }
 };
 
 let showProducts = () => {
@@ -206,3 +222,69 @@ const showErrorCard = (title, message) => {
     document.getElementById('error_toggler').style.right = '-350px';
   }, 2000);
 };
+document.getElementById('buscadorInput').addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    document.getElementById('buscadorButton').click();
+  }
+});
+document.getElementById('buscadorButton').addEventListener('click', () => {
+  let productoBuscado = productos.filter((producto) =>
+    producto.name.includes(document.getElementById('buscadorInput').value)
+  );
+  let acumulador = '';
+  productoBuscado.forEach((producto) => {
+    if (producto.discount == 0) {
+      acumulador += `
+      <div class="product">
+      <div class="product__name">
+        <h3>${producto.name}</h3>
+        </div>
+        <div class="product__price">
+          <div class="product__originalprice">
+            <p>${producto.price}</p>
+            </div>
+            <div class="product__discount">
+            <p>N/A</p>
+            </div>
+            </div>
+            <div class="product__description">
+            <p>${producto.desc}</p>
+            </div>
+            <div class="product__category">
+            <h3>${producto.category}</h3>
+            </div>
+            <a href="#" class="buy">Comprar</a>
+            </div>`;
+    } else {
+      let discountedPrice =
+        producto.price - (producto.price / 100) * producto.discount;
+      acumulador += `
+      <div class="product">
+        <div class="product__name">
+          <h3>${producto.name}</h3>
+          </div>
+        <div class="product__price_discounted">
+          <div class="product__originalprice">
+          <p>${producto.price}</p>
+          </div>
+          <div class="product__discount">
+            <p>${producto.discount}% off</p>
+            </div>
+            <div class="product__finalPrice">
+            <p>$${discountedPrice}</p>
+          </div>
+        </div>
+        <div class="product__description">
+          <p>${producto.desc}</p>
+        </div>
+        <div class="product__category">
+          <h3>${producto.category}</h3>
+        </div>
+        <a href="#" class="buy">Comprar</a>
+    </div>`;
+    }
+  });
+  divProductos.innerHTML = acumulador;
+});
+
+// document.getElementsByClassName("buy").forEach
