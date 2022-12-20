bbb8ac0dd15a019cc6cdc94cb3eef68f742b84a8
Agustin PC Casa <agustindiazmclh@gmail.com>
2022-12-19 23:30:40 -0300

    integracion responsive

--- Java Script/Entrega final/js/sliders.js
+++ Java Script/Entrega final/js/sliders.js
@@ -23,15 +23,15 @@ $(document).ready(function () {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       // You can unslick at a given breakpoint now by adding:
