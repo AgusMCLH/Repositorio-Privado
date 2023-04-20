c34c7df033cb10fa189b5160bc609e2391419a02
pache_dev <agustindiazmclh@gmail.com>
2023-04-19 22:41:58 -0300

    debbugg

--- BackEnd/Crypto y Fs/Codigo.js
+++ BackEnd/Crypto y Fs/Codigo.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
-const crypto = require('crypto');
-const { test } = require('node:test');
 
 const crearUsuario = async (user, Pass) => {
   try {
-    console.log('llega', user, Pass);
+    // console.log('llega', user, Pass);
     const usuarios = JSON.parse(
       await fs.promises.readFile('./files/usuarios.json', 'utf-8')
     );
+    const { createHash } = await import('node:crypto');
     const usuario = {
       user: user,
-      pass: await crypto.createHash('shake256', Pass).digest('hex'),
+      pass: await createHash('shake256').update(Pass, 'utf-8').digest('hex'),
     };
     console.log(await usuario);
     usuarios.push(await usuario);
@@ -36,11 +35,3 @@ const testing = async () => {
   await crearUsuario('pedro', 'pedro1234');
 };
 testing();
-
-// const Hash = async (pass) => {
-// const hash = await crypto.createHash();
-//   console.log(await hash);
-//   return await hash;
-// };
-
-// Hash('Agus1234');
---
+++ BackEnd/moment.js/code.js
@@ -0,0 +1,2 @@
+const moment = require('moment');
+console.log(moment().format('dddd, Do MMM YYYY, h:mm:ss a'));
