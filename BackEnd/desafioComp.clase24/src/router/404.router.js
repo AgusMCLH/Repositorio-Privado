3d7c68793d7d3befa36ef22e0566b6bdf350f6a0
pache_dev <agustindiazmclh@gmail.com>
2023-07-10 15:51:03 -0300

    fix (404 page): fixed button

--- BackEnd/desafioComp.clase24/src/router/404.router.js
+++ BackEnd/desafioComp.clase24/src/router/404.router.js
@@ -3,7 +3,8 @@ import { Router } from 'express';
 const notFoundPage = Router();
 
 notFoundPage.get('/', (req, res) => {
-  res.render('404page', { title: '404' });
+  const user = req.session.user;
+  res.render('404page', { title: '404', user });
 });
 
 export { notFoundPage };
