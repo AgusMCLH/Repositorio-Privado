41d11dd24ec54e10675fd4ea9b3b2efcf89c5204
pache_dev <agustindiazmclh@gmail.com>
2023-07-31 23:31:45 -0300

    Fix (Proy): Bug fixes

--- BackEnd/proyectoFinal.INCOMPLETO/src/app.js
+++ BackEnd/proyectoFinal.INCOMPLETO/src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 
-import config from '.././config/config.js';
+import config from './config/config.js';
 
-import { productGetPost } from './router/productGetPost.js';
-import { cartRouting } from './router/cartRouting.js';
 import { realTimeProducts } from './router/realtimeproduct.js';
 import { testRouter } from './router/test.router.js';
-import { userRouter } from './router/users.router.js';
-import { sessions } from './router/session.router.js';
 import { notFoundPage } from './router/404.router.js';
+import SessionRouter from './router/session.router.js';
+import ProductsRouter from './router/products.Router.js';
+import CartRouting from './router/carts.Router.js';
+import UserRouter from './router/users.Router.js';
+import ChatRouter from './router/chat.Router.js';
+import HomeRouter from './router/home.Router.js';
 
 import handlebars from 'express-handlebars';
 
@@ -21,6 +23,7 @@ import passport from 'passport';
 import initializePassport from './utils/passport.config.js';
 
 import { Server } from 'socket.io';
+let messages = [];
 
 // Declaro una app de tipo express y la configuro para que use JSON's y urlencoded ademas de usar la carpeta public para los archivos estaticos
 const app = express();
@@ -59,18 +62,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Rutas
-app.get('/', (req, res) => {
-  res.send(
-    '<br><p>Ingrese a <a href="/api/products">/api/products</a> para ver los productos</p><p>Ingrese a /api/carts/[id de su carrito] para ver su carrito</p><p>Ingrese a <a href="/api/realtimeproducts">/api/realtimeproducts</a> para ver los productos</p>'
-  );
-});
-
+app.use('/', new HomeRouter().getRouter());
 app.use('/api/test', testRouter);
-app.use('/api/products', productGetPost);
-app.use('/api/carts', cartRouting);
+app.use('/api/products', new ProductsRouter().getRouter());
+app.use('/api/chat', new ChatRouter().getRouter());
+app.use('/api/carts', new CartRouting().getRouter());
 app.use('/api/realtimeproducts', realTimeProducts);
-app.use('/users', userRouter);
-app.use('/api/sessions', sessions);
+app.use('/users', new UserRouter().getRouter());
+app.use('/api/sessions', new SessionRouter().getRouter());
 app.use('*', notFoundPage);
 
 // Configuro el puerto en el cual va a escuchar el servidor
@@ -81,3 +80,14 @@ const webServer = app.listen(config.PORT, () => {
 // Configuro socket.io
 const io = new Server(webServer);
 export { io };
+
+io.on('connection', (socket) => {
+  io.emit('ShowMessages', messages);
+  socket.on('NewMessage', (message) => {
+    console.log(message);
+    // Agrego el mensaje al array de mensajes
+    messages.push(message);
+    // Propago el evento a todos los clientes conectados
+    io.emit('ShowMessages', messages);
+  });
+});
