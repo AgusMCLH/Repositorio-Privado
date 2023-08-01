a0817e8cffe521de2e106ef32b39c3b0e7a04644
pache_dev <agustindiazmclh@gmail.com>
2023-07-31 23:26:14 -0300

    feat (Home): Home page creation

---
+++ BackEnd/proyectoFinal.INCOMPLETO/src/router/home.Router.js
@@ -0,0 +1,10 @@
+import CustomRouter from './customRouter/customRouter.js';
+
+export default class HomeRouter extends CustomRouter {
+  init() {
+    this.get('/', ['PUBLIC'], [], async (req, res) => {
+      const user = req.session.user;
+      res.render('home', { title: 'Home', user });
+    });
+  }
+}
