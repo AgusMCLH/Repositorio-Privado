70241824e6866fa45917075b9a58a59c5f35d252
pache_dev <agustindiazmclh@gmail.com>
2023-08-05 01:04:09 -0300

    Feat (Home) : Banner Text Animations

--- BackEnd/proyectoFinal.INCOMPLETO/public/js/home.js
+++ BackEnd/proyectoFinal.INCOMPLETO/public/js/home.js
@@ -4,8 +4,6 @@ const textStates = ['active-Text', 'unactive-Text'];
 const banners = document.getElementsByClassName('hero-img');
 const texts = document.getElementsByClassName('hero-text');
 
-console.log('texts', texts);
-
 let index;
 
 for (let i = 0; i < banners.length; i++) {
@@ -13,22 +11,28 @@ for (let i = 0; i < banners.length; i++) {
     index = i;
   }
 }
-console.log('el activo es', index);
 
 document.getElementById('prev-hero_button').addEventListener('click', () => {
+  console.log(index);
   banners[index].classList.remove(states[0]);
   banners[index].classList.add(states[1]);
 
   texts[index].classList.remove(textStates[0]);
   texts[index].classList.add(textStates[1]);
+
+  unactiveTexts(texts[index].children);
+
   index--;
   if (index < 0) {
     index = banners.length - 1;
   }
-  texts[index].classList.remove(textStates[1]);
-  texts[index].classList.add(textStates[0]);
+
+  activeTexts(texts[index].children);
   banners[index].classList.remove(states[1]);
   banners[index].classList.add(states[0]);
+
+  texts[index].classList.remove(textStates[1]);
+  texts[index].classList.add(textStates[0]);
 });
 
 document.getElementById('next-hero_button').addEventListener('click', () => {
@@ -45,3 +49,24 @@ document.getElementById('next-hero_button').addEventListener('click', () => {
   banners[index].classList.remove(states[1]);
   banners[index].classList.add(states[0]);
 });
+
+const activeTexts = (textList) => {
+  const chidren = textList[0].children;
+  for (let i = 0; i < chidren.length; i++) {
+    let classList = chidren[i].classList.value;
+    classList = classList.split('-');
+    classList = classList[0] + '-' + classList[1];
+    chidren[i].classList = classList + '-active';
+  }
+};
+
+const unactiveTexts = (textList) => {
+  const chidren = textList[0].children;
+  for (let i = 0; i < chidren.length; i++) {
+    let classList = chidren[i].classList.value;
+    classList = classList.split('-');
+    classList = classList[0] + '-' + classList[1];
+    console.log('classList es: ', classList);
+    chidren[i].classList = classList + '-unactive';
+  }
+};
