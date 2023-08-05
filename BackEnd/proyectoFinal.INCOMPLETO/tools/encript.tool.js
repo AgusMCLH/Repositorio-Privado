aa86800420f28ccbb5c8376742da18a066bcecf8
pache_dev <agustindiazmclh@gmail.com>
2023-08-05 18:08:56 -0300

    fix (Proy): Restructuration

--- BackEnd/proyectoFinal.INCOMPLETO/tools/encript.tool.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const encriptPassword = (password) => {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-};
-
-export const comparePassword = (password, passwordHash) => {
-  return bcrypt.compareSync(password, passwordHash);
-};
